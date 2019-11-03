b4805d8f47c923e7ae13ae26484196f3b90f30c5
Pulkit Gupta <pulkitgupta64@gmail.com>
2019-11-03 18:51:59 +0530

    ADD FILES

---
+++ lib/login.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class Login extends StatefulWidget {
+//   @override
+//   _LoginState createState() => _LoginState();
+// }
+
+// class _LoginState extends State<Login> {
+  
+//   final GoogleSignIn googleSignIn = GoogleSignIn();
+//   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+//   SharedPreferences preferences;
+//   bool loading= false;
+//   bool isLogedin = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+      
+//     );
+//   }
+// }
