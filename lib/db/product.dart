0faf53b3f786d24a09534b6a2dc02177e0cea988
Pulkit Gupta <pulkitgupta64@gmail.com>
2019-11-09 00:37:38 +0530

    ADDED MORE DATABASE WORK FOR CATEGORIES

---
+++ lib/db/product.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
+
+ class ProductService{
+   Firestore _firestore = Firestore.instance;
+   String ref = 'products';
+
+   void uploadProduct({String productName,String category, int price, int quantity, String image }){
+   var id = Uuid();
+   String productId = id.v1();
+
+  
+    _firestore.collection(ref).document(productId).setData({
+      'name': productName,
+      'id': productId,
+      'category': category,
+      'quantity': quantity,
+      'price': price,
+
+    });
+  
+   }
+ }
--- lib/registerproduct.dart
+++ lib/registerproduct.dart
@@ -1,23 +1,34 @@
 // home_material.dart
+import 'dart:io';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'db/category.dart';
+import 'db/product.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
- 
+import 'package:image_picker/image_picker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 class HomeMaterial extends StatefulWidget {
   @override
   _HomeMaterialState createState() => _HomeMaterialState();
 }
 class _HomeMaterialState extends State {
   CategoryService _categoryService = CategoryService();
+  ProductService _productService = ProductService();
+
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController _productNameController = TextEditingController();
+  TextEditingController _productPriceController = TextEditingController();
+  TextEditingController _productQuantityController = TextEditingController();
   List<DocumentSnapshot> categories = <DocumentSnapshot>[];
   List<DropdownMenuItem<String>> categoriesDropDown= <DropdownMenuItem<String>>[];
   String _currentCategory = "test";
   TextEditingController _productPrice = TextEditingController();
-  
+  File _image1;
+  bool isLoading = false;
   
   @override
   void initState(){
@@ -37,8 +48,6 @@ class _HomeMaterialState extends State {
     }
     return items;
   }
-  final _user = User();
-
 
   @override
   Widget build(BuildContext context) {
@@ -55,168 +64,176 @@ class _HomeMaterialState extends State {
               child: Builder(
                 builder: (context) => Form(
                   key: _formKey,
-                  child: Column(
+                  child: SingleChildScrollView(
+                  child: isLoading ? CircularProgressIndicator: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          controller: _productNameController,
-                          decoration:InputDecoration(labelText: 'Product Name'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter product name';
-                            }
-                          },
-                             // onSaved: (val) =>
-                             //   setState(() => _user.ProductName = val),
-                        ),
-                      ),
                       Row(
-                        children: <Widget>[ 
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('Category',style: TextStyle(color: Colors.red),
-                            ),
-                          ),
-                          Expanded(
-                            child:  DropdownButton(
-                              items: categoriesDropDown,
-                              value: _currentCategory,
-                              onChanged: changeSelectedCategory,
-                            ),
-                          )
-                         
-                        ],
-  
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlineButton(
+                        borderSide: BorderSide(color: Colors.grey.withOpacity(0.5), width: 2.5),
+                        onPressed: (){
+                          _selectImage(ImagePicker.pickImage(source: ImageSource.gallery), 1);
+                        },
+                        child: _displayChild1()
                       ),
-                      
-                     Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: TextFormField(
-                          controller: _productNameController,
-                          keyboardType: TextInputType.numberWithOptions(),
-                          decoration:InputDecoration(labelText: 'Product Price'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter product name';
-                            }
-                          },
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _productNameController,
+                  decoration:InputDecoration(labelText: 'Product Name'),
+                  validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter product name';
+                  }
+                },
+                            // onSaved: (val) =>
                              //   setState(() => _user.ProductName = val),
-                        ),
-                      ),           
-
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: TextFormField(
-                          controller: _productNameController,
-                          keyboardType: TextInputType.numberWithOptions(),
-                          decoration:InputDecoration(labelText: 'Quantity'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter product name';
-                            }
-                          },
-                             //   setState(() => _user.ProductName = val),
-                        ),
-                      ),           
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
-                        child: Text('Upload Image'),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
-                        child: Text('Product Category'),
-                      ),        
-                                
-                        //CheckboxListTile(
-                        //   title: const Text('Groceries & Staples'),
-                         //  value: _user.category[User.CategoryGrocery],
-                       //                                         onChanged: (val) {
-                                //                                           setState(() =>
-                                //                                           _user.category[User.CategoryGrocery] = val);
-                                //                                         }),
-                                //                                       CheckboxListTile(
-                                //                                         title: const Text('Personal Needs'),
-                                //                                         value: _user.category[User.CategoryPersonal],
-                                //                                         onChanged: (val) {
-                                //                                           setState(() => _user
-                                //                                            .category[User.CategoryPersonal] = val);
-                                //                                         }),
-                                //                                       CheckboxListTile(
-                                //                                           title: const Text('Household Needs'),
-                                //                                           value: _user.category[User.CategoryHousehold],
-                                //                                           onChanged: (val) {
-                                //                                             setState(() =>
-                                //                                             _user.category[User.CategoryHousehold] = val);
-                                //                                           }),
-                                //                                       CheckboxListTile(
-                                //                                           title: const Text('Snacks'),
-                                //                                           value: _user.category[User.CategorySnacks],
-                                //                                           onChanged: (val) {
-                                //                                             setState(() =>
-                                //                                             _user.category[User.CategorySnacks] = val);
-                                //                                           }),
-                                //                                       CheckboxListTile(
-                                //                                           title: const Text('Dairy Product'),
-                                //                                           value: _user.category[User.CategoryDairy],
-                                //                                           onChanged: (val) {
-                                //                                             setState(() =>
-                                //                                             _user.category[User.CategoryDairy] = val);
-                                //                                           }),
-                                //                                       CheckboxListTile(
-                                //                                           title: const Text('Frozen Food'),
-                                //                                           value: _user.category[User.CategoryFrozenFood],
-                                //                                           onChanged: (val) {
-                                //                                             setState(() =>
-                                //                                             _user.category[User.CategoryFrozenFood] = val);
-                                //                                           }),
-                                                                      /*
-                                                                  Container(
-                                                                      padding: const EdgeInsets.symmetric(
-                                                                          vertical: 16.0, horizontal: 16.0),
-                                                                      child: RaisedButton(
-                                                                          onPressed: () {
-                                                                            final form = _formKey.currentState;
-                                                                            if (form.validate()) {
-                                                                              form.save();
-                                                                              _user.save();
-                                                                              _showDialog(context);
-                                                                            }
-                                                                          },
-                                                                          child: Text('Save'))),*/
-                                                        
-                   FlatButton(
-                      color: Colors.red,
-                      textColor: Colors.white,
-                      child: Text('Add product'),
-                      onPressed: (){},
-                    )
-                  ]
+               ),
+              ),
+              Row(
+               children: <Widget>[ 
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Category',style: TextStyle(color: Colors.red),
+                   ),
+                ),
+                Expanded(
+                  child:  DropdownButton(
+                    items: categoriesDropDown,
+                    value: _currentCategory,
+                    onChanged: changeSelectedCategory,
+                  ),
                 )
-              )
-            )
-          
-        )
+                      
+              ],
+             ),
+                      
+             Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                controller: _productPriceController,
+               // initialValue: '0.00',
+                keyboardType: TextInputType.numberWithOptions(),
+                decoration:InputDecoration(labelText: 'Product Price'),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter product price';
+                  }
+                },
+              ),
+             ),           
+
+             Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                controller: _productQuantityController,
+              //  initialValue: '1',
+                keyboardType: TextInputType.numberWithOptions(),
+                decoration:InputDecoration(labelText: 'Quantity'),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter product Quantity';
+                  }
+                },
+                          //   setState(() => _user.ProductName = val),
+              ),
+            ),           
+                       
+                  FlatButton(
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    child: Text('Add product'),
+                    onPressed: (){
+                      validateAndUpload();
+                                          },
+                                        )
+                                      ]
+                                    )
+                                  )
+                 )
+                                )
+                             
+                              )
+                            );
+                          }
+  void _selectImage(Future<File> pickImage, int imageNumber) async{
+    File tempImg = await pickImage;
+    switch(imageNumber){
+      case 1:  setState(() => _image1 = tempImg);
+      break;
+    }
+  }
+  Widget _displayChild1() {
+    if(_image1 == null){
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(14, 70, 14, 70),
+        child: new Icon(Icons.add, color: Colors.grey,),
       );
     }
-   _showDialog(BuildContext context) {
-      Scaffold.of(context)
-      .showSnackBar(SnackBar(content: Text('Submitting form')));
+    else{
+      return Image.file(_image1, fit: BoxFit.fill, width: double.infinity,);
     }
-                                        
-  _getCategories() async{                   
-    List<DocumentSnapshot> data = await _categoryService.getCategories();
-    
-    print(data.length);
-    setState((){
+  }
+                         
+                                                              
+   _getCategories() async{                   
+      List<DocumentSnapshot> data = await _categoryService.getCategories();                         
+      print(data.length);
+      setState((){
       categories = data;
       categoriesDropDown = getCategoriesDropdown();
       _currentCategory = categories[0].data['category'];
-     });
-   }
-                                    
-  void changeSelectedCategory(String selectedCategory) {
+    });
+  }
+                                                          
+  void changeSelectedCategory(String selectedCategory) 
+  {
     setState(() => _currentCategory= selectedCategory);
+  }
+                      
+  void validateAndUpload() async{
+    if(_formKey.currentState.validate()){
+      setState(() =>
+        isLoading = true
+      );
+        if(_image1 != null){
+          String imageUrl1;
+          final FirebaseStorage storage = FirebaseStorage.instance;
+          final String picture1 = "${DateTime.now().millisecondsSinceEpoch.toString()}.jpg";
+          StorageUploadTask task1 =  storage.ref().child(picture1).putFile(_image1);
+          StorageTaskSnapshot snapshot1 = await task1.onComplete.then((snapshot)=>snapshot);
+          
+          task1.onComplete.then((snapshot1) async{
+
+            imageUrl1 = await snapshot1.ref.getDownloadURL();
+          _productService.uploadProduct(productName: _productNameController.text,
+                                        category: _currentCategory,
+                                        price: int.parse(_productPriceController.text),
+                                        quantity: int.parse(_productQuantityController.text), 
+                                        image: imageUrl1 );
+          _formKey.currentState.reset();
+      
+          setState(() => isLoading = false);
+
+          Fluttertoast.showToast(msg: 'Product added to the database');
+
+          Navigator.pop(context);
+            
+          });
+        }
+        else{
+           setState(() =>isLoading = false);
+          Fluttertoast.showToast(msg: 'at least one images must be provided');
+        }
+    }
+  
   }
 }
